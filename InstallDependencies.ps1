ae822defcab473f650bcac24bed65d63a58b4069
Daniel Libby <dlibby@microsoft.com>
2018-07-03 17:04:03 -0700

    Update InstallDependencies.ps1

--- InstallDependencies.ps1
+++ InstallDependencies.ps1
@@ -6,15 +6,16 @@ if (!(Test-Path "$RootDir\Downloads")) {
 
     # Download Files
     New-Item -ItemType Directory -Force -Path $RootDir\Downloads
-    Import-Module BitsTransfer
-    Start-BitsTransfer -Source $DepotToolsUrl -Destination "$RootDir\Downloads\depot_tools.zip"
-    Start-BitsTransfer -Source $VSSDKUrl -Destination "$RootDir\Downloads\winsdksetup.exe"
-
+    
+    $client = New-Object System.Net.WebClient 
+    $client.DownloadFile($DepotToolsUrl, "$RootDir\Downloads\depot_tools.zip") 
+    $client.DownloadFile($VSSDKUrl, "$RootDir\Downloads\winsdksetup.exe") 
+    
     # Install Depot Tools
     Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$RootDir\Downloads\depot_tools.zip", "$RootDir\DepotTools\")
 
     # Install Windows SDK
-    Start-Process -Wait "$RootDir\Downloads\winsdksetup.exe" "/features + /q" 
+    Start-Process -Wait "$RootDir\Downloads\winsdksetup.exe" "/features OptionId.WindowsDesktopDebuggers OptionId.DesktopCPPx64 OptionId.DesktopCPPx86 /q" 
     
     # fetch code
     New-Item -ItemType Directory -Force -Path $RootDir\src
